620d3283cc29d98fe96542ba0f44f6fffbb28ea8
Henrique Tavares <ihenrits@gmail.com>
2025-02-08 18:52:02 -0300

    feat: enhance HeroCard responsiveness with adaptive styles

--- src/components/hero-card/hero-card.tsx
+++ src/components/hero-card/hero-card.tsx
@@ -11,7 +11,7 @@ import { HeroCardProps } from './hero-card.types';
 
 export function HeroCard({ hero }: HeroCardProps) {
   return (
-    <div className="bg-gray-200 rounded-2xl h-[160px] w-full flex justify-center items-center">
+    <div className="max-[1440px]:h-[112px] bg-gray-200 rounded-2xl h-[160px] w-full flex justify-center items-center">
       <div className="flex flex-row items-start justify-between h-full">
         <div className="relative p-4 flex-shrink-0">
           <Image
@@ -19,7 +19,7 @@ export function HeroCard({ hero }: HeroCardProps) {
             alt={hero.name}
             width={121}
             height={121}
-            className="rounded-2xl"
+            className="rounded-2xl w-[100px] h-[100px] max-[1440px]:w-[80px] max-[1440px]:h-[80px]"
             quality={100}
           />
 
@@ -27,8 +27,12 @@ export function HeroCard({ hero }: HeroCardProps) {
         </div>
 
         <div className="flex flex-col py-4 flex-">
-          <h2 className="text-lg font-bold">{hero.name}</h2>
-          <p className="text-sm line-clamp-5">{hero.description}</p>
+          <h2 className="max-[1440px]:text-sm text-base font-bold">
+            {hero.name}
+          </h2>
+          <p className="max-[1440px]:text-xs max-[1440px]:line-clamp-3 text-sm line-clamp-4">
+            {hero.description}
+          </p>
         </div>
 
         <div className="flex flex-col p-4">
