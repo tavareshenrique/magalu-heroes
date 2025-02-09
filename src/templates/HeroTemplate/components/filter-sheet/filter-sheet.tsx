f284156d61e456af775195fe6a9684ff10432ce2
Henrique Tavares <ihenrits@gmail.com>
2025-02-09 09:56:32 -0300

    feat: enhance FilterSheet component with styled header and integrated Filter component

--- src/templates/HeroTemplate/components/filter-sheet/filter-sheet.tsx
+++ src/templates/HeroTemplate/components/filter-sheet/filter-sheet.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { useHeroes } from '@/context/HeroContext';
 import { useEffect, useState } from 'react';
 
 import { Sheet } from 'react-modal-sheet';
+
+import { useHeroes } from '@/context/HeroContext';
+
+import { Filter } from '@/components/filter/filter';
 
 export function FilterSheet() {
   const [isMounted, setIsMounted] = useState(false);
@@ -23,21 +26,18 @@ export function FilterSheet() {
       onClose={handleToggleBottomSheetFilter}
     >
       <Sheet.Backdrop className="bg-black/50" />
-      <Sheet.Container className="bg-white rounded-t-2xl shadow-lg p-4">
-        <Sheet.Header className="flex justify-center py-2">
-          <div className="w-12 h-1.5 bg-gray-400 rounded-full" />
+
+      <Sheet.Container className="bg-sky-550 rounded-t-2xl shadow-lg h-[50vh] max-h-[400px]">
+        <Sheet.Header className="flex justify-center py-2 bg-sky-550">
+          <div className="w-12 h-1.5 bg-white rounded-full" />
         </Sheet.Header>
-        <Sheet.Content className="p-4">
-          <h1 className="text-lg font-semibold text-gray-800">Olá</h1>
-          <p className="text-gray-600 mt-2">
-            Este é um bottom sheet estilizado com Tailwind.
-          </p>
-          <button
-            onClick={() => {}}
-            className="mt-4 w-full bg-blue-500 text-white py-2 rounded-lg shadow hover:bg-blue-600 transition"
-          >
-            Fechar
-          </button>
+
+        <Sheet.Content className="p-6 bg-sky-550 flex flex-col items-center justify-start">
+          <h1 className="text-lg font-extrabold text-white">Filter</h1>
+
+          <div>
+            <Filter />
+          </div>
         </Sheet.Content>
       </Sheet.Container>
     </Sheet>
