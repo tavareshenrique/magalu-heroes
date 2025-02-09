1324d3fe2deb6ca754a052bd8a37bd6cb4db76c4
Henrique Tavares <ihenrits@gmail.com>
2025-02-09 11:30:44 -0300

    refactor: simplify Hero component by removing hardcoded heroes data

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,56 +1,5 @@
 import { HeroTemplate } from '@/templates/HeroTemplate/HeroTemplate';
 
-const heroes = [
-  {
-    id: '1',
-    name: 'Spider-Man',
-    description:
-      'Peter Parker is a high school student and a superhero with spider-like abilities.',
-    thumbnail: '/images/spider.jpg',
-    isFavorite: false,
-  },
-  {
-    id: '2',
-    name: 'Spider-Man',
-    description:
-      'Peter Parker is a high school student and a superhero with spider-like abilities.',
-    thumbnail: '/images/spider.jpg',
-    isFavorite: false,
-  },
-  {
-    id: '3',
-    name: 'Spider-Man',
-    description:
-      'Peter Parker is a high school student and a superhero with spider-like abilities.',
-    thumbnail: '/images/spider.jpg',
-    isFavorite: false,
-  },
-  {
-    id: '4',
-    name: 'Spider-Man',
-    description:
-      'Peter Parker is a high school student and a superhero with spider-like abilities.',
-    thumbnail: '/images/spider.jpg',
-    isFavorite: false,
-  },
-  {
-    id: '5',
-    name: 'Spider-Man',
-    description:
-      'Peter Parker is a high school student and a superhero with spider-like abilities.',
-    thumbnail: '/images/spider.jpg',
-    isFavorite: false,
-  },
-  {
-    id: '6',
-    name: 'Spider-Man',
-    description:
-      'Peter Parker is a high school student and a superhero with spider-like abilities.',
-    thumbnail: '/images/spider.jpg',
-    isFavorite: false,
-  },
-];
-
-export default function Home() {
-  return <HeroTemplate heroes={heroes} />;
+export default function Hero() {
+  return <HeroTemplate />;
 }
