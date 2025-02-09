9c920fab08cccf3c727fd0007f68e026cd09157b
Henrique Tavares <ihenrits@gmail.com>
2025-02-09 11:52:46 -0300

    feat: add byName parameter to fetchHeroes for filtering by name

--- src/repositories/fetch-heroes.ts
+++ src/repositories/fetch-heroes.ts
@@ -6,15 +6,27 @@ import { FetchHeroesParams, HeroResponse } from './types/fetch-heroes.types';
 
 const LIMIT_PER_PAGE = 6;
 
-export async function fetchHeroes({ page, orderBy }: FetchHeroesParams) {
+export async function fetchHeroes({
+  page,
+  orderBy,
+  byName = null,
+}: FetchHeroesParams) {
   const offset = page * LIMIT_PER_PAGE;
 
+  const params = {
+    limit: LIMIT_PER_PAGE,
+    offset,
+    orderBy,
+  };
+
+  const nameStartsWith = byName ? { nameStartsWith: byName } : {};
+
+  Object.assign(params, nameStartsWith);
+
+  console.log(params);
+
   const response = await api.get<HeroResponse>('/characters', {
-    params: {
-      limit: LIMIT_PER_PAGE,
-      offset,
-      orderBy,
-    },
+    params,
   });
 
   const heroes = makeFetchHeroes(response.data);
