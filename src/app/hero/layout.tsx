440cc177d0cd68aa0926fcf26fb1f09fb296180f
Henrique Tavares <ihenrits@gmail.com>
2025-02-09 09:56:44 -0300

    feat: replace manual filter inputs with Filter component in HeroLayout

--- src/app/hero/layout.tsx
+++ src/app/hero/layout.tsx
@@ -1,7 +1,6 @@
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Switch } from '@/components/ui/switch';
 import Image from 'next/image';
+
+import { Filter } from '@/components/filter/filter';
 
 const ALT_TITLE =
   "A silhouette of a superhero all in white flying with his right hand pointing to the sky, symbolizing the application's logo.";
@@ -16,7 +15,7 @@ export default function HeroLayout({
       <section className="md:w-[392px] md:bg-sky-550 h-full">
         <div className="h-[241px] bg-sky-650 flex items-center justify-center">
           <Image
-            src="/images/logo.svg"
+            src="/images/logo.png"
             alt={ALT_TITLE}
             title={ALT_TITLE}
             width={170}
@@ -28,23 +27,7 @@ export default function HeroLayout({
         <div className="md:block p-14 hidden">
           <h1 className="text-2xl font-extrabold text-white mb-6">Filters</h1>
 
-          <div className="flex flex-col items-start space-y-2 mt-8">
-            <Label htmlFor="hero-name" className="text-white font-bold">
-              Hero Name:
-            </Label>
-            <Input
-              id="hero-name"
-              type="text"
-              placeholder="Tell me the hero's name..."
-            />
-          </div>
-
-          <div className="flex flex-col items-start space-y-2 mt-8">
-            <Label htmlFor="hero-name" className="text-white font-bold">
-              Show only favorites?
-            </Label>
-            <Switch />
-          </div>
+          <Filter />
         </div>
       </section>
 
