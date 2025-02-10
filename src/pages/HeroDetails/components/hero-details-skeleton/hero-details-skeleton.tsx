d8c666cba8fc0e4bd01286e8705ed8fec7316753
Henrique Tavares <ihenrits@gmail.com>
2025-02-10 10:52:19 -0300

    feat: add HeroDetailsSkeleton component for loading state representation

---
+++ src/pages/HeroDetails/components/hero-details-skeleton/hero-details-skeleton.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+
+import { Skeleton } from '@/components/ui/skeleton';
+
+export function HeroDetailsSkeleton() {
+  return (
+    <>
+      <div className="w-full max-h-[200px] overflow-hidden md:hidden">
+        <Skeleton className="w-full h-52" />
+      </div>
+
+      <div className="md:px-14 md:mt-10 mt-4 px-6">
+        <Skeleton className="w-10 h-10" />
+
+        <div className="flex flex-row mt-5 md:mt-10">
+          <div className="relative hidden md:block">
+            <Skeleton className="w-48 h-48" />
+          </div>
+
+          <div className="flex flex-col items-start md:ml-4 w-full md:mx-4 md:mt-0">
+            <div className="flex flex-row items-center justify-between w-full">
+              <Skeleton className="mr-4 md:mr-0 w-full h-10" />
+
+              <Skeleton className="ml-4 w-10 h-10" />
+            </div>
+
+            <div className="w-full mt-4">
+              <Skeleton className="mr-4 md:mr-4 mt-2 w-full h-32" />
+            </div>
+          </div>
+        </div>
+
+        <div className="max-[1440px]:mt-2 mt-8 flex flex-row items-center justify-start">
+          <Image
+            src="/images/magazine-luiza.webp"
+            alt="Lu da Magalu"
+            width={48}
+            height={48}
+            className="rounded-full mr-2 w-auto h-auto max-w-[10%] max-[1440px]:w-[4%] sm:max-w-[48px]"
+            quality={10}
+          />
+
+          <Skeleton className="w-full h-10" />
+        </div>
+
+        <Skeleton className="h-10 w-full max-[1440px]:mt-4 mt-10" />
+      </div>
+    </>
+  );
+}
