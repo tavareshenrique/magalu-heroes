4083979ef49dfc41230c74a3b5af6f2b3576d632
Henrique Tavares <ihenrits@gmail.com>
2025-02-10 11:22:31 -0300

    feat: enhance AllHeroes component with loading animation and pagination support

--- src/pages/Hero/components/all-heroes/all-heroes.tsx
+++ src/pages/Hero/components/all-heroes/all-heroes.tsx
@@ -1,29 +1,70 @@
 'use client';
 
-import { HeroCard } from '@/components/hero-card/hero-card';
+import dynamic from 'next/dynamic';
 
-import { AllHeroesProps } from './all-heroes.types';
 import { useHeroes } from '@/context/HeroContext';
 
-export function AllHeroes({ heroes }: AllHeroesProps) {
+import { HeroCard } from '@/components/hero-card/hero-card';
+import { SimplePagination } from '@/components/simple-pagination/simple-pagination';
+
+import { AllHeroesProps } from './all-heroes.types';
+
+import * as heroLottie from '@/assets/lotties/hero-lottie.json';
+
+const EmptyData = dynamic(
+  () =>
+    import('@/components/animation-lottie/animation-lottie').then(
+      (mod) => mod.AnimationLottie,
+    ),
+  {
+    ssr: false,
+  },
+);
+
+export function AllHeroes({
+  heroes,
+  isLastPage,
+  page,
+  setPage,
+}: AllHeroesProps) {
   const { onAddFavoriteHero, onRemoveFavoriteHero } = useHeroes();
 
-  return (
-    <div className="mt-8">
-      <h1 className="text-xl font-extrabold text-sky-900 mb-6">All</h1>
+  if (heroes.length === 0) {
+    return (
+      <div className="mt-8 h-full">
+        <h1 className="text-xl font-extrabold text-sky-900 mb-6">All</h1>
 
-      <div className="relative">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {heroes.map((hero) => (
-            <HeroCard
-              key={hero.id}
-              hero={hero}
-              onAddFavoriteHero={onAddFavoriteHero}
-              onRemoveFavoriteHero={onRemoveFavoriteHero}
-            />
-          ))}
+        <div className="flex flex-col justify-center items-center h-full">
+          <EmptyData animationData={heroLottie} size={300} />
+
+          <p className="text-2xl mt-4 mb-8 text-sky-900 font-bold">
+            No characters found...
+          </p>
         </div>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="mt-8">
+        <h1 className="text-xl font-extrabold text-sky-900 mb-6">All</h1>
+
+        <div className="relative">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {heroes.map((hero) => (
+              <HeroCard
+                key={hero.id}
+                hero={hero}
+                onAddFavoriteHero={onAddFavoriteHero}
+                onRemoveFavoriteHero={onRemoveFavoriteHero}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <SimplePagination page={page} setPage={setPage} isLastPage={isLastPage} />
+    </>
   );
 }
--- src/pages/Hero/components/all-heroes/all-heroes.types.ts
+++ src/pages/Hero/components/all-heroes/all-heroes.types.ts
@@ -2,4 +2,7 @@ import { Hero } from '@/types/global.types';
 
 export interface AllHeroesProps {
   heroes: Hero[];
+  page: number;
+  isLastPage: boolean;
+  setPage: (page: number) => void;
 }
