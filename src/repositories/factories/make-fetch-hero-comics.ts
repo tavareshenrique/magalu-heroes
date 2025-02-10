5af47ca9297223ed3e59f609f0e216cc87ca8dcd
Henrique Tavares <ihenrits@gmail.com>
2025-02-10 10:52:10 -0300

    feat: add makeFetchHeroComics function for fetching and parsing hero comic data

---
+++ src/repositories/factories/make-fetch-hero-comics.ts
@@ -0,0 +1,34 @@
+import { isLastPage } from '@/utils/isLastPage';
+
+import { Comic } from '@/types/global.types';
+import { ComicResponse } from '../types/fetch-hero-comics.types';
+
+async function makeFetchHeroComics(comics: ComicResponse) {
+  const parsedComics: Comic[] = comics.data.results.map((hero) => {
+    const { id, title, description, thumbnail } = hero;
+
+    const parsedId = id.toString();
+
+    const thumbnailPath = thumbnail.path + '.' + thumbnail.extension;
+
+    return {
+      id: parsedId,
+      title,
+      description: description || 'No description available...',
+      thumbnail: thumbnailPath,
+    };
+  });
+
+  const isLast = isLastPage(
+    comics.data.offset,
+    comics.data.count,
+    comics.data.total,
+  );
+
+  return {
+    comics: parsedComics,
+    isLastPage: isLast,
+  };
+}
+
+export { makeFetchHeroComics };
