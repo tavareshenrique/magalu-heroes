e5d3c58d3a57b2551da0d0499eb6277f261320c7
Henrique Tavares <ihenrits@gmail.com>
2025-02-10 08:06:57 -0300

    feat: add HeroResponse and Result interfaces for hero data structure

--- src/types/global.types.ts
+++ src/types/global.types.ts
@@ -5,3 +5,26 @@ export type Hero = {
   thumbnail: string;
   isFavorite?: boolean;
 };
+
+export interface HeroResponse {
+  data: {
+    offset: number;
+    limit: number;
+    total: number;
+    count: number;
+    results: Result[];
+  };
+}
+
+export interface Result {
+  id: number;
+  name: string;
+  description: string;
+  modified: string;
+  thumbnail: Thumbnail;
+}
+
+export interface Thumbnail {
+  path: string;
+  extension: string;
+}
