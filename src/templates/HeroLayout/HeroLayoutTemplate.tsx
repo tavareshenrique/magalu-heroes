c311a4962209b77dd4590600fc6afac33416aa16
Henrique Tavares <ihenrits@gmail.com>
2025-02-10 14:53:31 -0300

    feat: add HeroLayoutTemplate and related components for enhanced layout structure

---
+++ src/templates/HeroLayout/HeroLayoutTemplate.tsx
@@ -0,0 +1,17 @@
+import { HeroLayoutTemplateProps } from './components/HeroLayoutTemplate.types';
+import { LayoutFilter } from './components/layout-filter/layout-filter';
+import { LayoutLogo } from './components/layout-logo/layout-logo';
+
+export function HeroLayoutTemplate({ children }: HeroLayoutTemplateProps) {
+  return (
+    <div className="md:flex-row md:h-screen flex flex-col">
+      <section className="md:w-[392px] md:bg-sky-550 h-full">
+        <LayoutLogo />
+
+        <LayoutFilter />
+      </section>
+
+      <main className="flex-1">{children}</main>
+    </div>
+  );
+}
---
+++ src/templates/HeroLayout/components/layout-filter/layout-filter.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { Suspense } from 'react';
+
+import { Filter } from '@/components/filter/filter';
+
+export function LayoutFilter() {
+  const pathname = usePathname();
+
+  const isHeroDetailPage = /^\/hero\/\d+$/.test(pathname!);
+
+  return (
+    <div
+      className={`md:block p-14 hidden ${isHeroDetailPage && 'hidden md:hidden'}`}
+    >
+      <h1 className="text-2xl font-extrabold text-white mb-6">Filters</h1>
+
+      <Suspense>
+        <Filter />
+      </Suspense>
+    </div>
+  );
+}
