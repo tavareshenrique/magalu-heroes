8e23f7b56d98ccbef2cfb6383062e1128e7c40be
Henrique Tavares <ihenrits@gmail.com>
2025-02-10 08:58:15 -0300

    feat: replace logo image with NavImage component in HeroLayout

--- src/app/hero/layout.tsx
+++ src/app/hero/layout.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image';
-
 import { NavFilter } from '@/components/nav-filter/nav-filter';
-
-const ALT_TITLE =
-  "A silhouette of a superhero all in white flying with his right hand pointing to the sky, symbolizing the application's logo.";
+import { NavImage } from '@/components/nav-image/nav-image';
 
 export default async function HeroLayout({
   children,
@@ -13,21 +9,12 @@ export default async function HeroLayout({
   return (
     <div className="md:flex-row md:h-screen flex flex-col">
       <section className="md:w-[392px] md:bg-sky-550 h-full">
-        <div className="h-[241px] bg-sky-650 flex items-center justify-center">
-          <Image
-            src="/images/logo.png"
-            alt={ALT_TITLE}
-            title={ALT_TITLE}
-            width={170}
-            height={170}
-            quality={100}
-          />
-        </div>
+        <NavImage />
 
         <NavFilter />
       </section>
 
-      <main className="md:px-14 md:mt-10 flex-1 mt-4 px-6">{children}</main>
+      <main className="flex-1">{children}</main>
     </div>
   );
 }
