ff7be85cfed035db5f56f5c15e1927072c3e1c23
Henrique Tavares <ihenrits@gmail.com>
2025-02-11 14:29:43 -0300

    feat: add data-testid attribute to AllComicsSkeleton for improved testing

--- src/routes/HeroDetails/components/all-comics/all-comics.skeleton.tsx
+++ src/routes/HeroDetails/components/all-comics/all-comics.skeleton.tsx
@@ -2,7 +2,10 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export function AllComicsSkeleton() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div
+      data-testid="all-comics-skeleton"
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
+    >
       {Array.from({ length: 6 }).map((_, index) => (
         <Skeleton
           className="max-[1440px]:h-[112px] rounded-2xl h-[160px] w-full"
