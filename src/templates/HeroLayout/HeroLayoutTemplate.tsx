e330430f2058749b394c5f0e4a983981fe9fcccb
Henrique Tavares <ihenrits@gmail.com>
2025-02-11 09:51:43 -0300

    refactor: integrate QueryClientProvider in HeroLayoutTemplate and remove it from HeroTemplate for improved data management

--- src/templates/HeroLayout/HeroLayoutTemplate.tsx
+++ src/templates/HeroLayout/HeroLayoutTemplate.tsx
@@ -1,17 +1,28 @@
+'use client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import { queryClient } from '@/lib/react-query';
+
 import { LayoutFilter } from './components/layout-filter/layout-filter';
 import { LayoutLogo } from './components/layout-logo/layout-logo';
+import { LayoutStatistic } from './components/layout-statistic/layout-statistic';
 import { HeroLayoutTemplateProps } from './HeroLayoutTemplate.types';
 
 export function HeroLayoutTemplate({ children }: HeroLayoutTemplateProps) {
   return (
-    <div className="md:flex-row md:h-screen flex flex-col">
-      <section className="md:w-[392px] md:bg-sky-550 h-full">
-        <LayoutLogo />
+    <QueryClientProvider client={queryClient}>
+      <div className="md:flex-row md:h-screen flex flex-col">
+        <section className="md:w-[392px] md:bg-sky-550 h-full">
+          <LayoutLogo />
 
-        <LayoutFilter />
-      </section>
+          <LayoutFilter />
 
-      <main className="flex-1">{children}</main>
-    </div>
+          <LayoutStatistic />
+        </section>
+
+        <main className="flex-1">{children}</main>
+      </div>
+    </QueryClientProvider>
   );
 }
--- src/templates/HeroTemplate/HeroTemplate.tsx
+++ src/templates/HeroTemplate/HeroTemplate.tsx
@@ -1,18 +1,9 @@
 'use client';
 
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 import { HeroProvider } from '@/context/HeroContext';
-import { queryClient } from '@/lib/react-query';
 
 import { HeroTemplateProps } from './HeroTemplate.types';
 
 export function HeroTemplate({ children }: HeroTemplateProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <HeroProvider>{children}</HeroProvider>
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
-  );
+  return <HeroProvider>{children}</HeroProvider>;
 }
